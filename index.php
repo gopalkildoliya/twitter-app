1e8ec8f51d0375bbbefc500ba74c1dfe817ddd28
Gopal Kildoliya <gopalkildoliya@gmail.com>
2015-08-20 15:07:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<?php
-require "vendor/autoload.php";
 
-use Abraham\TwitterOAuth\TwitterOAuth;
-define('CONSUMER_KEY', 'Kx9KV3IUAn35bBhpAKHRdpnSf');
-define('CONSUMER_SECRET', 'rUpGE1NEthAEA9h8IcVDP5Se5xapXzTQbmTc8vJu1ZTJkBIKsN');
-define('OAUTH_CALLBACK', 'https://gopaltapp.herokuapp.com/authorize.php');
-
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
-$_SESSION['oauth_token'] = $request_token['oauth_token'];
-$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-?>
 <html>
 	<head>
 		<link type="text/css" rel="stylesheet" href="css/bootstrap.css">
@@ -20,7 +7,8 @@ $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token
 		<div class="container">
 			<div class="row">
 				<div class="col-md-2 col-md-offset-5" style="position:fixed; top:45%">
-					<a href="<?php echo $url;?>" class="btn btn-primary">Start with Twitter</a>
+					<a href="" class="btn btn-primary">Start with Twitter</a>
+					<?php echo 'hello hello'; ?>
 				</div>
 			</div>
 		</div>
