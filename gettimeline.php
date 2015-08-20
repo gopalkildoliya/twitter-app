399ab2313f0c977fbc896b04bd64d16c926451e4
gopalkildoliya <gopalkildoliya@gmail.com>
2015-08-20 18:25:47 +0530

    updated getttimeline

--- gettimeline.php
+++ gettimeline.php
@@ -13,5 +13,18 @@
 	$access_token = $_SESSION['access_token'];
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 	$timeline = $connection->get("statuses/home_timeline");
-	var_dump($timeline);
+	foreach ($timeline as $story) {
+		$user=$story->user;
+		echo '<div class="media">
+  				<div class="media-left">
+    				<a href="#">
+      					<img class="media-object" src="..." alt="...">
+    				</a>
+  				</div>
+  				<div class="media-body">
+    			<h4 class="media-heading">'.$user->name;.'</h4>'.
+    				$story->text;
+  				.'</div>
+			</div>'
+	}
 ?>
