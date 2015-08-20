96777fb4042df64bad990b119048c14d8edf6da9
gopalkildoliya <gopalkildoliya@gmail.com>
2015-08-20 15:21:21 +0530

    inkjbhhjg

--- index.php
+++ index.php
@@ -1,31 +1,16 @@
 <?php
-try {
-	session_start();
-} catch (Exception $e) {
-	echo 'Caught exception Session Start: ',  $e->getMessage(), "\n";
-}
-
-try {
-	require "vendor/autoload.php";
-} catch (Exception $e) {
-	echo 'Caught exception loading vendor/ autoload: ',  $e->getMessage(), "\n";
-}
-
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+require "vendor/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 define('CONSUMER_KEY', 'Kx9KV3IUAn35bBhpAKHRdpnSf');
 define('CONSUMER_SECRET', 'rUpGE1NEthAEA9h8IcVDP5Se5xapXzTQbmTc8vJu1ZTJkBIKsN');
 define('OAUTH_CALLBACK', 'https://gopaltapp.herokuapp.com/authorize.php');
 
-try {
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
-} catch (Exception $e) {
-	echo 'Caught exception first connection and request token: ',  $e->getMessage(), "\n";
-}
-
-
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
 $_SESSION['oauth_token'] = $request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
